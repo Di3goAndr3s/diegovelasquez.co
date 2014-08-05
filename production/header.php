3f55adeaa67f065980d12d486f855e6992caa306
diegovelasquez <diego.velasquez@imaginamos.co>
2014-08-05 17:53:07 -0500

    upload

---
+++ production/header.php
@@ -0,0 +1,17 @@
+
+<header>
+  <div class="wrapper"><a href="index.php" class="logo">
+      <div><i class="fa fa-plus fa-3x"></i>
+      </div>
+      <div>DV</div>
+      <div>Front-end Developer</div></a>
+    <nav>
+      <ul>
+        <li><a href="diegovelasquez.php"><i class="fa fa-user fa-lg"></i>Acerca de</a></li>
+        <li><a href="blog.php"><i class="fa fa-file-text-o fa-lg"></i>Blog</a></li>
+        <li><a href="videos.php"><i class="fa fa-video-camera fa-lg"></i>Videos</a></li>
+        <li><a href="contacto.php"><i class="fa fa-envelope-o fa-lg"></i>Contacto</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
